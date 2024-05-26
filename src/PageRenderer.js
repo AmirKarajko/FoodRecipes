1a6dcafe2c4ac67af26ef09e6fec8da7751a8b9d
AmirKarajko <amir.karajko@gmail.com>
2024-05-26 19:48:54 +0200

    #9 Implement Pagination Feature

--- src/PageRenderer.js
+++ src/PageRenderer.js
@@ -9,7 +9,9 @@ class PageRenderer extends Component {
             recipeId: null,
             recipesData: [],
             filteredRecipesData: [],
-            error: null
+            error: null,
+            currentPageNumber: 1,
+            recipesPerPage: 5,
         };
     }
 
@@ -19,86 +21,97 @@ class PageRenderer extends Component {
 
     fetchRecipesData = () => {
         fetch('http://localhost:5000/api/recipes')
-        .then(response => response.json())
-        .then(recipesData => { 
-            this.setState({ 
-                recipesData, 
-                filteredRecipesData: recipesData,
-                error: null 
-            }) 
-        })
-        .catch(error => console.error('Error fetching data:', error));
-    }
-
-    setCurrentPage = (pageName, id) => {
-        this.setState({ currentPage: pageName, recipeId: id });
+            .then(response => response.json())
+            .then(recipesData => {
+                this.setState({
+                    recipesData,
+                    filteredRecipesData: recipesData,
+                    error: null
+                })
+            })
+            .catch(error => console.error('Error fetching data:', error));
     }
 
     handleSearch = (e) => {
         const query = e.target.value.toLowerCase();
         const { recipesData } = this.state;
-        const filteredRecipesData = recipesData.filter(recipe => 
+        const filteredRecipesData = recipesData.filter(recipe =>
             recipe.recipe_name.toLowerCase().includes(query)
         );
         this.setState({ filteredRecipesData });
     }
 
+    handlePageChange = (pageNumber) => {
+        this.setState({ currentPageNumber: pageNumber });
+    }
+
     render() {
-        const { currentPage, filteredRecipesData } = this.state;
+        const { currentPage, filteredRecipesData, currentPageNumber, recipesPerPage } = this.state;
+
+        const indexOfLastRecipe = currentPageNumber * recipesPerPage;
+        const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
+        const currentRecipes = filteredRecipesData.slice(indexOfFirstRecipe, indexOfLastRecipe);
 
         let pageContent, tableContent;
         switch (currentPage) {
             case 'home':
                 tableContent =
-                <table>
-                <thead>
-                    <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {filteredRecipesData.map(recipe => (
-                    <tr key={recipe.recipe_id}>
-                        <td>{recipe.recipe_id}</td>
-                        <td>{recipe.recipe_name}</td>
-                        <td>
-                            <button onClick={() => this.setCurrentPage('recipes', recipe.recipe_id)}>View</button>
-                        </td>
-                    </tr>
-                ))}
-                </tbody>
-                </table>;
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {currentRecipes.map(recipe => (
+                                <tr key={recipe.recipe_id}>
+                                    <td>{recipe.recipe_id}</td>
+                                    <td>{recipe.recipe_name}</td>
+                                    <td>
+                                        <button onClick={() => this.setState({ currentPage: 'recipes', recipeId: recipe.recipe_id })}>View</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>;
 
-                pageContent = 
-                <div>
-                    <h1>Food Recipes</h1>
+                pageContent =
                     <div>
-                        <input type="text" placeholder="Search" onChange={this.handleSearch} />
-                    </div>
-                    {tableContent}
-                </div>;
+                        <h1>Food Recipes</h1>
+                        <div>
+                            <input type="text" placeholder="Search" onChange={this.handleSearch} />
+                        </div>
+                        {tableContent}
+                        
+                        <Pagination
+                            recipesPerPage={recipesPerPage}
+                            totalRecipes={filteredRecipesData.length}
+                            currentPage={currentPageNumber}
+                            onPageChange={this.handlePageChange}
+                        />
+                    </div>;
                 break;
             case 'recipes':
                 let recipeItem = filteredRecipesData.find(item => item.recipe_id === this.state.recipeId);
 
                 pageContent =
-                <div>
-                    <h1>Food Recipes</h1>
-                    <p>ID: {recipeItem.recipe_id}<br />
-                    Name: {recipeItem.recipe_name}<br />
-                    Category: {recipeItem.recipe_category}<br /><br />
-                    Instructions: <br /><br />{recipeItem.recipe_instruction}<br /><br />
-                    Ingredients:</p>
-                    <ul>
-                        {recipeItem.ingredients.map((ingredient, index) => (
-                            <li key={index}>
-                                {ingredient.ingredient_quantity} of {ingredient.ingredient_name}
-                            </li>
-                        ))}
-                    </ul>
-                </div>;
+                    <div>
+                        <h1>Food Recipes</h1>
+                        <p>ID: {recipeItem.recipe_id}<br />
+                            Name: {recipeItem.recipe_name}<br />
+                            Category: {recipeItem.recipe_category}<br /><br />
+                            Instructions: <br /><br />{recipeItem.recipe_instruction}<br /><br />
+                            Ingredients:</p>
+                        <ul>
+                            {recipeItem.ingredients.map((ingredient, index) => (
+                                <li key={index}>
+                                    {ingredient.ingredient_quantity} of {ingredient.ingredient_name}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>;
                 break;
             default:
                 break;
@@ -112,6 +125,20 @@ class PageRenderer extends Component {
             </div>
         );
     }
+}
+
+const Pagination = ({ recipesPerPage, totalRecipes, currentPage, onPageChange }) => {
+    const pageNumbers = Math.ceil(totalRecipes / recipesPerPage);
+
+    if (pageNumbers === 1) return null;
+
+    return (
+        <nav>
+            {Array.from({ length: pageNumbers }, (_, index) => (
+                <button key={index + 1} onClick={() => onPageChange(index + 1)} className={`pagination-btn ${currentPage === index + 1 ? 'active' : ''}`}>{index + 1}</button>
+            ))}
+        </nav>
+    );    
 };
 
 export default PageRenderer;
