d52ea36f8e95261915f442d4be2992cc97208d1a
AmirKarajko <amir.karajko@gmail.com>
2024-06-02 23:08:06 +0200

    #19 Remove App Files

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+
+import PageRenderer from './PageRenderer';
+import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PageRenderer />
   </React.StrictMode>
 );
 
