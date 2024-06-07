8d5db685a26b8779b4a66e4dd15742711c08a64b
AmirKarajko <amir.karajko@gmail.com>
2024-06-07 14:51:42 +0200

    #40 Rename Home to Dashboard

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import HomePage from './pages/home';
+import DashboardPage from './pages/dashboard';
 import RecipesPage from './pages/recipes';
 import AddNewRecipePage from './pages/add_new_recipe';
 import DeleteRecipePage from './pages/delete_recipe';
@@ -10,7 +10,7 @@ const App = () => {
     return (
         <Router>
             <Routes>
-                <Route exact path="/" element={<HomePage />} />
+                <Route exact path="/" element={<DashboardPage />} />
                 <Route path="/recipes/:id" element={<RecipesPage />} />
                 <Route path="/add_new_recipe" element={<AddNewRecipePage />} />
                 <Route path="/delete_recipe/:id" element={<DeleteRecipePage />} />
--- client/src/js/components/navbar.js
+++ client/src/js/components/navbar.js
@@ -4,10 +4,8 @@ function Navbar () {
     return (
         <nav>
             <ul className="topnav">
-            <li><a className="active" href="/">Home</a></li>
+            <li><a className="active" href="/">Dashboard</a></li>
             <li><a href="/add_new_recipe">Add New Recipe</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
     );
